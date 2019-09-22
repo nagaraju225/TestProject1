73a14e3243e9066fcedd3af812db3617e7df84fa
NagarajuRGV <NagarajuRGV@NagarajuRGV-PC>
2019-09-22 02:49:18 -0500

    my changes

--- 1HibernateTest/src/main/java/com/naga/Test1.java
+++ 1HibernateTest/src/main/java/com/naga/Test1.java
@@ -3,7 +3,7 @@ package com.naga;
 public class Test1 {
 
 	public static void main(String[] args) {
-		System.out.println("HI Mama this is my first song ");
+		System.out.println("HI Mama this is my first song ---123");
 	}
 	
 	
